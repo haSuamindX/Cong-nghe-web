919c792314008632a92deec0953485ad930e6e9a
Hasua118 <hongha1182000@gmail.com>
2021-09-26 23:35:43 +0700

    update homework

---
+++ cnweb/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./index.css">
+  <link href="https://pro.fontawesome.com/releases/v5.12.0/css/all.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="col-sm-1"></div>
+    <a class="navbar-brand" href="#"><img src="./img/logo.png" alt=""></a>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="col-sm-8"></div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item ">
+          <a class="nav-link" href="#" style="color: rgb(240, 76, 125); font-weight: 500">Home <span
+              class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: rgb(240, 76, 125) ; font-weight: 500">Categories</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: rgb(240, 76, 125); font-weight: 500">Foods</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li> -->
+        <li class="nav-item">
+          <a class="nav-link " href="#" style="color: rgb(240, 76, 125); font-weight: 500">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="row header">
+    <div class="col-sm-2"></div>
+    <div class="col-sm-8 centering">
+      <div class="input-group centering ">
+        <div class="form-outline" style="width: 30%">
+          <input type="search" id="form1" class="form-control" placeholder="Searching ..." style="width: 100%" />
+        </div>
+        <button type="button" class="btn btn-primary"
+          style="margin-left: 10px; width: 80px; background-color: rgb(240, 76, 125)">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </div>
+    <div class="col-sm-2"></div>
+  </div>
+
+  <div class="container-fluid">
+    <h1 class="text-center mt-5 mb-5">Explore Foods</h1>
+    ​ <div class="d-flex justify-content-around">
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 350px; height: 500px; background-image: url(./img/burger.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">Burger</h2>
+      </div>
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 350px; height: 500px; background-image: url(./img/pizza.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">Burger</h2>
+      </div>
+
+      <div class="d-flex align-items-end justify-content-center" style="width: 350px; height: 500px; background-image: url(./img/momo.jpg); border-radius: 10px;">
+        <h2 style="z-index:9; color: white">Burger</h2>
+      </div>
+      
+    </div>
+  </div>
+
+  <div class="container-fuild pt-5" style="margin-top: 80px; background-color: rgb(179, 179, 179)">
+    <h2 class="text-center">Food Menu</h2>
+
+    <div class="d-flex justify-content-around" style="width: 80%; margin: 50px auto;">
+      
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4>Dumplings Specials</h4>
+            <h4>$5.00</h4>
+            <p>Checken dumpling with herbs from Mountains</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0">Order now</button>
+          </div>
+      </div>
+
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4>Dumplings Specials</h4>
+            <h4>$5.00</h4>
+            <p>Checken dumpling with herbs from Mountains</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0">Order now</button>
+          </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-around pb-5" style="width: 80%; margin: 50px auto; ">
+      
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4>Dumplings Specials</h4>
+            <h4>$5.00</h4>
+            <p>Checken dumpling with herbs from Mountains</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0">Order now</button>
+          </div>
+      </div>
+
+      <div class="d-flex justify-content-around pb-3" style="width: 40%; background-color: white; padding-top: 15px; border-radius: 10px">
+          <img src="./img/menu-momo.jpg" alt="" style="width: 80px; height: 80px">
+          <div class="d-flex flex-column">
+            <h4>Dumplings Specials</h4>
+            <h4>$5.00</h4>
+            <p>Checken dumpling with herbs from Mountains</p>
+            <button type="button" class="btn btn-danger" style="width: 80px; padding: 0">Order now</button>
+          </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid d-flex justify-content-center">
+    <div class=" d-flex justify-content-around" style=" width: 40%">
+      <i class="fab fa-facebook" style="color: blue; font-size: 40px"></i>
+      <i class="fab fa-instagram" style="color: red; font-size: 40px"></i>
+      <i class="fab fa-twitter" style="color: blue; font-size: 40px"></i>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
+  </script>
+</body>
+
+</html>
